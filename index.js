d6be55615d84afb1c01853d907c00689249e768e
Lendl Peralta <janlendl@gmail.com>
2021-08-22 01:21:48 +0000

    tested the flyBy and it worked

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const { 
+const {
   fetchMyIP,
-  fetchCoordsByIP
- } = require('./iss');
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes
+} = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
 //     console.log('It didn\'t work!: ', error);
-//     return; 
+//     return;
 //   }
 
 //   console.log('It worked! Returned IP: ', ip);
 // });
 
-fetchCoordsByIP('Invalid', (error, data) => {
+// fetchCoordsByIP('Invalid', (error, coordinates) => {
+//   if (error) {
+//     console.log('It didn\'t work!: ', error);
+//     return;
+//   }
+//   console.log(error, coordinates);
+// });
+
+const coords = { latitude: '51.0927', longitude : '-114.2017'};
+
+fetchISSFlyOverTimes(coords, (error, flyBy) => {
   if (error) {
     console.log('It didn\'t work!: ', error);
     return;
   }
-  console.log(error, data);
+  console.log('It worked! Return flyover times: ', flyBy);
 });
