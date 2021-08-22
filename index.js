63dbaf9177a70d6cdc9fb48c9d0e1b6a44a2bd05
Lendl Peralta <janlendl@gmail.com>
2021-08-22 21:29:52 +0000

    created function to call the function at iss.js where it merged all 3 functions via callback. Also created a function to update the passby to be properly read

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const {
   fetchMyIP,
   fetchCoordsByIP,
-  fetchISSFlyOverTimes
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation
 } = require('./iss');
 
 // fetchMyIP((error, ip) => {
@@ -13,7 +14,7 @@ const {
 //   console.log('It worked! Returned IP: ', ip);
 // });
 
-// fetchCoordsByIP('Invalid', (error, coordinates) => {
+// fetchCoordsByIP('ip', (error, coordinates) => {
 //   if (error) {
 //     console.log('It didn\'t work!: ', error);
 //     return;
@@ -21,12 +22,28 @@ const {
 //   console.log(error, coordinates);
 // });
 
-const coords = { latitude: '51.0927', longitude : '-114.2017'};
+// const coords = { latitude: '51.0927', longitude : '-114.2017'};
 
-fetchISSFlyOverTimes(coords, (error, flyBy) => {
+// fetchISSFlyOverTimes(coords, (error, flyBy) => {
+//   if (error) {
+//     console.log('It didn\'t work!: ', error);
+//     return;
+//   }
+//   console.log('It worked! Return flyover times: ', flyBy);
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log('It didn\'t work!: ', error);
-    return;
+    return console.log('It didn\'t work! ', error);
   }
-  console.log('It worked! Return flyover times: ', flyBy);
+  printPassTime(passTimes);
 });
+
+const printPassTime = passTimes => {
+  passTimes.forEach(element => {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(element.risetime);
+    const duration = element.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+  });
+};
